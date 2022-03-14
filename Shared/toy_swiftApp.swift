334c90652581c29ff90924bc57121ec9dc0caffd
YeSifan <504936436@qq.com>
2022-03-14 20:11:11 +0800

    Initial Commit

---
+++ Shared/toy_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  toy_swiftApp.swift
+//  Shared
+//
+//  Created by 叶思凡 on 2022/3/14.
+//
+
+import SwiftUI
+
+@main
+struct toy_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
